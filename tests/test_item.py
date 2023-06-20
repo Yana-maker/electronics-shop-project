77aae58f2f88db412b2fe5d90f9c41b6298b72eb
yana <yanaposelenceva@gmail.com>
2023-06-20 09:06:17 +0300

    fixes

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,25 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
+from src.item import Item
+
+
+def test_init():
+    test = Item("1", 2, 3)
+    assert test.name == '1'
+    assert test.price == 2
+    assert test.quantity == 3
+
+def test_alculate_total_price():
+    test = Item("1", 2, 3)
+    assert test.calculate_total_price() == 6
+
+def test_apply_discount():
+    test = Item("1", 2, 3)
+
+    assert test.pay_rate * test.price == 2
+    test.pay_rate = 0.8
+    assert test.pay_rate * test.price == 1.6
+
+
+
