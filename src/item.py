2230b38d648ad9ca3ecbe0a96e9d1586d855db4e
yana <yanaposelenceva@gmail.com>
2023-06-22 21:46:20 +0300

    complete HW2

--- src/item.py
+++ src/item.py
@@ -1,9 +1,11 @@
+import csv
 class Item:
     """
     Класс для представления товара в магазине.
     """
     pay_rate = 1.0
     all = []
+
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
@@ -13,23 +15,57 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
-
         self.all.append(self)
 
 
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
-
         :return: Общая стоимость товара.
         """
-        return self.price * self.quantity
+        total = self.price * self.quantity
+        return total
+
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
         self.price = self.price * self.pay_rate
+        return self.price
+
+
+
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        cls.all.clear()
+        # with open(path, newline='') as csvfile:
+        with open('../src/items.csv', newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                item = cls(row['name'], row['price'], row['quantity'])
+
+
+
+    @classmethod
+    def string_to_number(cls, value):
+        float_value = float(value)
+        return int(float_value)
+
+
+    @property
+    def name(self):
+        return self.__name
+
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and value.isalpha():
+            if len(value) <=10:
+                self.__name = value
+            else:
+                self.__name = value[:10]
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,8 +1,6 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
-import pytest
 
 from src.item import Item
-
 
 def test_init():
     test = Item("1", 2, 3)
@@ -10,16 +8,35 @@ def test_init():
     assert test.price == 2
     assert test.quantity == 3
 
-def test_alculate_total_price():
+def test_property():
+    test = Item("1", 2, 3)
+    assert test.name == '1'
+    test.name = '3'
+    assert test.name == '1'
+
+def test_setter():
+    test = Item("1", 2, 3)
+    test.name = ('fdsfdsfsdfsd')
+    assert test.name == 'fdsfdsfsdf'
+
+
+def test_calculate_total_price():
     test = Item("1", 2, 3)
     assert test.calculate_total_price() == 6
 
 def test_apply_discount():
     test = Item("1", 2, 3)
+    assert test.apply_discount() == 2
+    test.pay_rate = 1.5
+    assert test.apply_discount() == 3
 
-    assert test.pay_rate * test.price == 2
-    test.pay_rate = 0.8
-    assert test.pay_rate * test.price == 1.6
+def test_instantiate_from_csv():
+    test = Item("1", 2, 3)
+    test.instantiate_from_csv()
+    assert len(test.all) == 5
 
 
-
+def test_string_to_number():
+    test = Item("1", 2, 3)
+    assert test.string_to_number('6') == 6
+    assert test.string_to_number('5.5') == 5
