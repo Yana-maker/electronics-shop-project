177fcd18adf931274257e44f8ccf743907b94e54
yana <yanaposelenceva@gmail.com>
2023-06-27 22:08:48 +0300

    completed HW4

--- src/phone.py
+++ src/phone.py
@@ -2,6 +2,8 @@ from src.item import Item
 
 
 class Phone(Item):
+
+
     """Phone содержит все атрибуты класса `Item` и дополнительно атрибут,
     содержащий количество поддерживаемых сим - карт"""
     def __init__(self, name, price, quantity, number_of_sim):
@@ -12,3 +14,16 @@ class Phone(Item):
         """repr для класса Phone"""
         return f"{self.__class__.__name__}('{self.name}', " \
                f"{self.price}, {self.quantity}, {self.number_of_sim})"
+
+    def number_of_sim(self):
+        if self.number_of_sim <= 0:
+            raise ValueError
+        else:
+            return self.number_of_sim
+
+
+phone1 = Phone("iPhone 14", 120_000, 5, 0)
+
+print(phone1.number_of_sim)
+
+
