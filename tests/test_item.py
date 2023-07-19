6596b66f2ca11dccb28f3ce0e984ff4b4cc31f28
yana <yanaposelenceva@gmail.com>
2023-07-19 13:46:28 +0300

    completed tests HW6

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,13 +1,11 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 import pytest
-from src.item import Item
-
+from src.item import Item, InstantiateCSVError
 
 @pytest.fixture
 def item():
     item = Item("1", 2, 3)
     return item
-
 
 def test_init(item):
     assert item.name == "1"
@@ -15,35 +13,39 @@ def test_init(item):
     assert item.quantity == 3
 
 def test_property(item):
-
     assert item.name == '1'
     item.name = '3'
     assert item.name == '1'
 
 def test_setter(item):
-
     item.name = ('fdsfdsfsdfsd')
     assert item.name == 'fdsfdsfsdf'
 
-
 def test_calculate_total_price(item):
-
     assert item.calculate_total_price() == 6
 
 def test_apply_discount(item):
-
     assert item.apply_discount() == 2
     item.pay_rate = 1.5
     assert item.apply_discount() == 3
 
 def test_instantiate_from_csv(item):
-
     item.instantiate_from_csv()
     assert len(item.all) == 5
 
+def test_instantiate_from_csv_file_exist(item):
+    with pytest.raises(FileNotFoundError):
+        item.instantiate_from_csv()
+        if open('../src/items2.csv'):
+            raise FileNotFoundError
+
+def test_InstantiateCSVError(item):
+    with pytest.raises(InstantiateCSVError):
+        item.instantiate_from_csv()
+        if len('../src/items.csv') != 3:
+            raise InstantiateCSVError
 
 def test_string_to_number(item):
-
     assert item.string_to_number('6') == 6
     assert item.string_to_number('5.5') == 5
 
